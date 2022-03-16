4fa4b0e76e9d7925e528c5685ef205d38e604a42
coltonward07 <coltonward7767@gmail.com>
2022-03-15 20:29:23 -0400

    Login components

--- src/Login/login.js
+++ src/Login/login.js
@@ -1,8 +1,51 @@
-import React from "react";
-class Login extends React.Component{
-    render{}}
-        return{
+import React from 'react';
+import styled from 'styled-components';
 
+const BoxContainer = styled.div'
+    width: 200px;
+    min-height: 550px;
+    display: flex;
+    flex-direction: column;
+    border-radius: 15px;
+    background-color: #fff;
+    box-shadow: 0 0 2px rgba{15, 15, 0,28};
+    position: relative;
+    overflow: hidden;
+}
 
-        }
+const TopContainer = styled.div
+    width: 100%;
+    height: 250px;
+}
+const BackDrop = styled.div
+    width: 160%;
+    height: 550px;
+    position: absoulute;
+    display: flex;
+    flex-direction: column;
+    border-radius: 50px;
+}
+    false; 
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export function AccountBox{props} {
+
 }
